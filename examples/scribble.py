664f8117675069405b18dbd1c058b6570a2347ed
Henri Tuhola <henri.tuhola@gmail.com>
2009-02-18 21:52:12 +0200

    couldn't resist the temptation to write a scribble

---
+++ examples/scribble.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from __future__ import with_statement
+from contextlib import nested
+
+import pyglet
+from pyglet.gl import *
+from gletools import Framebuffer, Texture
+
+window = pyglet.window.Window()
+texture = Texture(window.width, window.height)
+framebuffer = Framebuffer()
+framebuffer.textures[0] = texture
+
+def quad(left, right, top, bottom):
+    glBegin(GL_QUADS)
+    glTexCoord2f(1.0, 1.0)
+    glVertex3f(right, top, 0.0)
+    glTexCoord2f(1.0, 0.0)
+    glVertex3f(right, bottom, 0.0)
+    glTexCoord2f(0.0, 0.0)
+    glVertex3f(left, bottom, 0.0)
+    glTexCoord2f(0.0, 1.0)
+    glVertex3f(left, top, 0.0)
+    glEnd()
+
+@window.event
+def on_mouse_drag(x, y, rx, ry, button, modifier):
+    glEnable(GL_POINT_SMOOTH)
+    glEnable(GL_LINE_SMOOTH)
+    if pyglet.window.mouse.LEFT == button:
+        glColor4f(1,1,1,1)
+        glPointSize(3)
+        glLineWidth(3)
+    else:
+        glColor4f(0,0,0,0)
+        glPointSize(24)
+        glLineWidth(24)
+    with nested(framebuffer):
+        glBegin(GL_LINES)
+        glVertex3f(x, y, 0)
+        glVertex3f(x-rx, y-ry, 0)
+        glEnd()
+        glBegin(GL_POINTS)
+        glVertex3f(x, y, 0)
+        glVertex3f(x-rx, y-ry, 0)
+        glEnd()
+    glDisable(GL_POINT_SMOOTH)
+    glDisable(GL_LINE_SMOOTH)
+
+@window.event
+def on_draw():
+    window.clear()
+    glColor4f(1,1,1,1)
+    with texture:
+        quad(left=0, right=texture.width, top=texture.height, bottom=0)
+
+if __name__=='__main__':
+    pyglet.app.run()
